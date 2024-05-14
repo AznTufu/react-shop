fe9b2afe5ae1b217034d7f49e1fa96246361226c
Tony Zhang <zhgtony.pro@hotmail.com>
2024-05-14 23:17:15 +0200

    :lipstick:

--- src/pages/ProductsScreen.jsx
+++ src/pages/ProductsScreen.jsx
@@ -33,7 +33,7 @@ export default function ProductsScreen() {
         <h1>Products</h1>
         <ProductsContainer>
           {
-          !isLoading && data ? data.map((product) => (
+          !isLoading && data && data.length > 0 ? data.map((product) => (
               <Product
                 key={product.id} 
                 product={product} 
